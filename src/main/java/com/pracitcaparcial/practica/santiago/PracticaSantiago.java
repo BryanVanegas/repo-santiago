c6c457378c0afed06b0ca88b8eb80545a36a589b
bryanvanegas <bryan.steveen98@hotmail.com>
2025-09-03 16:35:57 -0500

    Primero commit.

---
+++ src/main/java/com/pracitcaparcial/practica/santiago/PracticaSantiago.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pracitcaparcial.practica.santiago;
+
+/**
+ *
+ * @author BryanVanegas
+ */
+public class PracticaSantiago {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
